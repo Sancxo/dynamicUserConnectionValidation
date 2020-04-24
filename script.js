ab22c34c4b69bae98ccab00b86104571f4e51596
Sancxo <58341133+Sancxo@users.noreply.github.com>
2020-04-24 12:50:01 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+const email = document.getElementById('email');
+const emailIsValid = document.getElementById('emailIsValid');
+
+const password = document.getElementById('password');
+const passwordIsValid = document.getElementById('passwordIsValid');
+
+const connectButton = document.getElementById('connectButton');
+
+const emailCheck = email.addEventListener('change', function (e) {
+    const value = e.target.value;
+    if (value.includes('@') && value.includes('.')) {
+        emailIsValid.setAttribute('style', 'color:green');
+        emailIsValid.innerHTML = 'Email valid.';
+        password.disabled = false;
+    } else {
+        emailIsValid.setAttribute('style', 'color:red');
+        emailIsValid.innerHTML = 'Email not valid !';
+    };
+});
+
+var passwordCheck = password.addEventListener('change', function (e) {
+    const value = e.target.value;
+    if (value.includes('-') || value.includes('_')) {
+        passwordIsValid.setAttribute('style', 'color:green');
+        passwordIsValid.innerHTML = 'Password valid.';
+        connectButton.disabled = false;
+    } else {
+        passwordIsValid.setAttribute('style', 'color:red');
+        passwordIsValid.innerHTML = 'Password not valid !';
+    };
+});
